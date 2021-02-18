b4f4d02a69e4b58698c547902a91400d0b82cf73
Ahmed Zain Al-Hamawi <78156960+azain95@users.noreply.github.com>
2021-02-18 14:36:17 +0300

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import logo from "./aayedi.png";
 import Produclist from "./component/Produclist";
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
   return (
     <div>
-            <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wlcome to Ayedi boutique
-        </p>
-       
+        <p>Wlcome to Ayedi boutique</p>
       </header>
- <a>New Items</a> 
+      <a>New Items</a>
+
       <Produclist />
-    <footer>
-    <a
+      <footer>
+        <a
           className="App-link"
           href="https://instagram.com/ayedi_boutique?igshid=jxzlrsexlczt"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Back To Instagram
+          Back To Instagram
         </a>
-      </footer>  
+      </footer>
     </div>
-    
-    
   );
 }
 

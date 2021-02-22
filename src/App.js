7c9a7c53967ef5dd224b4e647b15178e86ecc886
Ahmed Zain Al-Hamawi <78156960+azain95@users.noreply.github.com>
2021-02-22 17:18:40 +0300

    styling for dark mode

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { useState } from "react";
 
-import { GlobalStyle, ThemeButton, Titale } from "./styles2";
+import { GlobalStyle, ThemeButton, Titale, AA } from "./styles2";
 
 function App() {
   const theme = {
     light: {
       mainColor: "#242424", // main font color
       backgroundColor: "#fefafb", // main background color
-      tect: "#ff85a2",
+      headersColor: "#473d8b",
     },
     dark: {
       mainColor: "#fefafb", // main font color
       backgroundColor: "#242424", // main background color
-      pink: "#ff85a2",
+      headersColor: "#00d5ff",
     },
   };
   const [currentTheme, setCurrentTheme] = useState("light");
@@ -33,8 +33,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Titale>{"Wlcome to Ayedi boutique "}</Titale>
       </header>
-      <a>New Items</a>
-
+      <AA>New Items</AA>
       <Produclist />
       <footer>
         <a
--- src/styles2.js
+++ src/styles2.js
@@ -2,17 +2,22 @@ import styled, { createGlobalStyle } from "styled-components";
 const Titale = styled.h1`
   align-items: center;
   justify-content: center;
-  color: darkslateblue;
+  color: ${(props) => props.theme.headersColor};
   font-family: "Aclonica", sans-serif;
   font-size: calc(25px + 7vmin);
 `;
 const GlobalStyle = createGlobalStyle`
+
   body {
     font-family: "Aclonica", sans-serif;
-
+    
     background-color: ${(props) => props.theme.backgroundColor};
     color:${(props) => props.theme.mainColor};
   }`;
+
+const AA = styled.a`
+  color: ${(props) => props.theme.headersColor};
+`;
 
 const ThemeButton = styled.button`
   font-size: 1em;
@@ -23,5 +28,14 @@ const ThemeButton = styled.button`
   background-color: ${(props) => props.theme.mainColor};
   color: ${(props) => props.theme.backgroundColor};
 `;
+const logoheader = styled.header`
+  background-color: ${(props) => props.theme.mainColor};
+  min-height: 75vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(25px + 2vmin);
+`;
 
-export { Titale, GlobalStyle, ThemeButton };
+export { Titale, GlobalStyle, ThemeButton, AA };
